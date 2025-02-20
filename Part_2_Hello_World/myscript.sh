be5252f0781fcadbfbb22d2abea1e16a4ab653e8
Daniel Di Nardi <dan.dinardi@hotmail.com>
2025-02-20 15:41:20 +0000

    Completed parts 2, 3, and continuing 5

---
+++ Part_2_Hello_World/myscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello World"
+
+echo "My Current working directory is:"
+pwd
---
+++ Part_3_Variables/myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+name="Your Name"
+now=$(date)
+
+echo "Hello $name"
+echo "The system time and date is: $now"
+echo "Your username is: $USER"

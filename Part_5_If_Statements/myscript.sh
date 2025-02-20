33fc70e1e3621dccb5d8e6b67f061d2daa152959
Daniel Di Nardi <dan.dinardi@hotmail.com>
2025-02-20 16:00:23 +0000

    Completed Part 5

--- Part_5_If_Statements/myscript.sh
+++ Part_5_If_Statements/myscript.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-command=/usr/bin/htop
+command=htop
 
-if [ -f $command ]
+#[] == "test" command
+# "command" checks for the existance of a command
+if command -v $command
 then
-    echo "$command is available..."
+	echo "$command is available, let's run it..."
 else
-    echo "$command is not available, installing it..."
-    sudo apt update && sudo apt install -y htop
+	echo "$command is not available, installing it..."
+	sudo apt update && sudo apt install -y $command
 fi
 
 $command

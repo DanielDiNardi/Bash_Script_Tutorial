4dc75139a9464066a51d29031deb71f6e3355145
Daniel Di Nardi <dan.dinardi@hotmail.com>
2025-02-23 15:05:06 +0000

    Completed Part 11, updated the 'update' script from Part 8 and created a script that gets user input

--- Part_8_Universal_Update_Script/update_original.sh
+++ Part_8_Universal_Update_Script/update_original.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file
 then 
 	# The host is based on Arch, run the pacman update command
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
 fi
 
 if  grep -q "ubuntu" $release_file || grep -q "debian" $release_file
 then
 	# The host is running on Debian or Ubuntu,
 	# Run the apt version of the command
-	sudo apt update
-	sudo apt dist-upgrade
-	sudo apt autoremove
+	sudo apt update 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
+	sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
+	sudo apt autoremove -y 1>>$logfile 2>>$errorlog
+	if [ $? -ne 0 ]
+	then
+		echo "An error occurred, please check the $errorlog file."
+	fi
 fi

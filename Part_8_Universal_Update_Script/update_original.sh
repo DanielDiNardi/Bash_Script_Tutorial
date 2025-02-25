9343c0cc5c8247483a145abae55215243d7a05b9
Daniel Di Nardi <dan.dinardi@hotmail.com>
2025-02-25 10:04:27 +0000

    Completed Part 12 and added a check exit status function to the update script

--- Part_8_Universal_Update_Script/update_original.sh
+++ Part_8_Universal_Update_Script/update_original.sh
@@ -4,14 +4,18 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
-if grep -q "Arch" $release_file
-then 
-	# The host is based on Arch, run the pacman update command
-	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+check_exit_status() {
 	if [ $? -ne 0 ]
 	then
 		echo "An error occurred, please check the $errorlog file."
 	fi
+}
+
+if grep -q "Arch" $release_file
+then 
+	# The host is based on Arch, run the pacman update command
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	check_exit_status
 fi
 
 if  grep -q "ubuntu" $release_file || grep -q "debian" $release_file
@@ -19,18 +23,9 @@ then
 	# The host is running on Debian or Ubuntu,
 	# Run the apt version of the command
 	sudo apt update 1>>$logfile 2>>$errorlog
-	if [ $? -ne 0 ]
-	then
-		echo "An error occurred, please check the $errorlog file."
-	fi
+	check_exit_status
 	sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
-	if [ $? -ne 0 ]
-	then
-		echo "An error occurred, please check the $errorlog file."
-	fi
+	check_exit_status
 	sudo apt autoremove -y 1>>$logfile 2>>$errorlog
-	if [ $? -ne 0 ]
-	then
-		echo "An error occurred, please check the $errorlog file."
-	fi
+	check_exit_status
 fi
